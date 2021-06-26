46015ed884c681c0eb5ef71a9712b631fafc4852
harsha21111 <harshavardan2k17@gmail.com>
2021-06-26 17:18:11 +0530

    swagger addittions

--- server.js
+++ server.js
@@ -1,5 +1,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+swaggerJsdoc = require("swagger-jsdoc"),
+swaggerUi = require("swagger-ui-express");
+
+const options = {
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "Node-Express",
+        version: "0.1.0",
+        description:
+          "This is a simple CRUD API application made with Express and documented with Swagger",
+        license: {
+          name: "ISC",
+          url: "",
+        },
+        contact: {
+          name: "harsha",
+          url: "https://google.com",
+          email: "google123@email.com",
+        },
+      },
+      servers: [
+        {
+          url: "http://localhost:3000",
+        },
+      ],
+    },
+    apis: ['./app/routes/note.routes.js'],
+  };
+  
 
 const app = express();
 
@@ -26,6 +56,13 @@ app.get('/', (req, res) => {
 });
 
 require('./app/routes/note.routes.js')(app);
+
+const specs = swaggerJsdoc(options);
+  app.use(
+    "/notes1",
+    swaggerUi.serve,
+    swaggerUi.setup(specs, { explorer: true })
+  );
 
 app.listen(3000, () => {
     console.log("Server is listening on port 3000");
